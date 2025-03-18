f9eb6b18557b5d66ec410108452c6258ab6f5e73
wanderson-vicente <wandersonvicente@gmail.com>
2025-03-18 17:17:11 -0300

    Function adicionarAmigo- 1 teste

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Inicialização da lista de amigos
+let listaDeAmigos = [];
+
+// Função para adicionar um amigo à lista
+function adicionarAmigo() {
+  // Obter o valor do campo de entrada
+  const inputAmigo = document.getElementById('amigo');
+  const nomeAmigo = inputAmigo.value.trim();
+  
+  
+  // Adicionar o nome à lista
+  listaDeAmigos.push(nomeAmigo);
+  
+ console.log(listaDeAmigos);
+   
+
+}
